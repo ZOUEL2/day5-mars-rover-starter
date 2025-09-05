603b0e29915f4c58f33cb1797fb980c052541401
ZOUEL2 <zouel2@oocl.com>
2025-09-05 16:03:30 +0800

    feat: marsRover move method implement

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -2,21 +2,46 @@ package com.afs.tdd;
 
 public class MarsRover {
 
-    Location Location = new Location(0, 0, Direction.N);
+    private Location location;
 
-    public void executeCommand(Command m) {
-
-
+    public MarsRover(Location location) {
+        this.location = location;
     }
-
-    public void move(){
-
-    }
-
 
     public Location getLocation() {
-        return Location;
+        return location;
     }
+
+    public String executeCommand(Command command) {
+        if (command == Command.M){
+            move(location);
+        }
+
+        StringBuilder result = new StringBuilder();
+        result.append(location.getLocationX()).append(":")
+                .append(location.getLocationY()).append(":")
+                .append(location.getDirection());
+        return result.toString();
+    }
+
+    public void move(Location location) {
+        switch (location.getDirection()) {
+            case N:
+                location.setLocationY(location.getLocationY() + 1);
+                break;
+            case W:
+                location.setLocationX(location.getLocationX() - 1);
+                break;
+            case E:
+                location.setLocationX(location.getLocationX() + 1);
+                break;
+            case S:
+                location.setLocationY(location.getLocationY() - 1);
+                break;
+        }
+    }
+
+
 
 
 }
--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -10,17 +10,15 @@ public class MarsRoverTest {
     @Test
     void should_move_when_executeCommand_given_M() {
         Location location = new Location(0, 0, Direction.N);
-        MarsRover marsRover = new MarsRover();
+        MarsRover marsRover = new MarsRover(location);
 
         marsRover.executeCommand(Command.M);
 
-        Location nextLocation = new Location(0,1,Direction.N);
+        Location nextLocation = new Location(0, 1, Direction.N);
 
-        assertEquals(nextLocation.getLocationX(),marsRover.getLocation().getLocationX());
-        assertEquals(nextLocation.getLocationY(),marsRover.getLocation().getLocationY());
-        assertEquals(nextLocation.getDirection(),marsRover.getLocation().getDirection());
-
-
+        assertEquals(nextLocation.getLocationX(), marsRover.getLocation().getLocationX());
+        assertEquals(nextLocation.getLocationY(), marsRover.getLocation().getLocationY());
+        assertEquals(nextLocation.getDirection(), marsRover.getLocation().getDirection());
     }
 
 
